8643b0358ed0d4781cd46214174e95c43bec90f4
hafizhaua <72615421+hafizhaua@users.noreply.github.com>
2020-10-09 23:26:14 +0800

    Update Zambezi_Adu Loncatan Jangkrik.c

--- TLS/Zambezi_Adu Loncatan Jangkrik.c
+++ TLS/Zambezi_Adu Loncatan Jangkrik.c
@@ -7,7 +7,7 @@ int main(){
 	//input processes
 	scanf("%d %d %d %d", &x1, &v1, &x2, &v2);
 
-	if(x1 == x2){					//case 1 : x1 = x2
+	if(x1 == x2){			//case 1 : x1 = x2
 		if(v1 != v2){
 			x1++;	
 		}
